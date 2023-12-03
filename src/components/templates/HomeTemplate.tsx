14847439b92dbee29b5e1f544de5ada9a5d417ba
yoshifumi-kondo <yoshifumi.kondo1993@gmail.com>
2023-12-03 16:18:22 +0900

    Refactor HomeTemplate component layout

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -1,10 +1,13 @@
+import { DallE3Interface } from "@/components/organisms/DallEV3_Interface";
 import { GptV3_5TurboInterface } from "@/components/organisms/GptV3_5TurboInterface";
 import { FC } from "react";
 
 export const HomeTemplate: FC = () => {
   return (
-    <main className="flex h-screen w-screen justify-center items-center flex-wrap">
-      <GptV3_5TurboInterface />
+    <main className="flex h-screen w-screen justify-center items-center">
+      <div className="flex justify-center w-full">
+        <GptV3_5TurboInterface />
+      </div>
     </main>
   );
 };
